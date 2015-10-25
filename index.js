c5b5808ae168ba7cbfcd44fdd01021fd08d906a6
Shamasis Bhattacharya <mail@shamasis.net>
2015-10-25 20:31:20 +0530

    Improved code towards production release. Also updated package file and added tests.

--- index.js
+++ index.js
@@ -1,14 +1,80 @@
-var pkgInfo = require('./lib/packageinfo').
-    semver = require('semver');
+var _ = require('lodash'),
+    $ = require('async'),
+    semver = require('semver'),
+    fs = require('fs'),
+    joinpath = require('path').join,
+    resolvepath = require('path').resolve,
+
+    HIDDEN_FILE_REGEX = /^[^\.]/,
+    REQUIRED_PACKAGE_DATA = ['name', 'version', 'dependencies', 'devDependencies'],
+    MODULE_FOLDER_NAME = 'node_modules',
+    PACKAGE_FILE_NAME = 'package.json',
+    PACKAGE_NAME_KEY = 'name',
+
+    defaultOptions = {
+        path: '',
+        d: false
+    };
 
 module.exports = function (options, callback) {
-    !options && (options = {});
+    $.waterfall([
+        // prepare options
+        function (done) {
+            options = _.defaults(options || {}, defaultOptions);
 
-    pkgInfo(options.path, function (err, packages) {
-	if (err) { return callback(err); }
+            // do not allow if path is not provided
+            if (!options.path) {
+                return done(new Error('packity: invalid module path: "' + options.path + '"'));
+            }
 
-	// ensure all dependencies are present
-	
-    });
+            done();
+        },
 
+        // fetch node module directories
+        function (done) {
+            fs.readdir(resolvepath(options.path, MODULE_FOLDER_NAME), function (err, dirs) {
+                // remove hidden directories
+                done(err, !err && _.filter(dirs, RegExp.prototype.test, HIDDEN_FILE_REGEX));
+            });
+        },
+
+        // transform the directories to paths of package.json
+        function (dirs, done) {
+            $.map(dirs, function (dir, next) {
+                next(null, resolvepath(options.path, MODULE_FOLDER_NAME, dir, PACKAGE_FILE_NAME));
+            }, done);
+        },
+
+        // reduce the package file paths to the ones that exist
+        function (files, done) {
+            $.filter(files, fs.exists, _.bind(done, this, null));
+        },
+
+        // get all package file data in each module directory
+        function (files, done) {
+            $.map(files, function (file, next) {
+                next(null, _.pick(require(file), REQUIRED_PACKAGE_DATA));
+            }, done);
+        },
+
+        // load root package and return result to callback
+        function (modules, done) {
+            done(null, _.pick(require(resolvepath(options.path, PACKAGE_FILE_NAME)), REQUIRED_PACKAGE_DATA), 
+                _.indexBy(modules, PACKAGE_NAME_KEY));
+        },
+
+        // check status for dependencies and dev dependencies defined within package with respect to installed
+        // modules
+        function (package, modules, done) {
+            done(null, {
+                package: package,
+                installed: modules,
+                // while caluclating status merge dependencies and dev dependencies if options are specified
+                status: _.mapValues(_.merge(package.dependencies, options.d && package.devDependencies), 
+                    function (version, dependency) {
+                        return modules[dependency] ? semver.satisfies(modules[dependency].version, version) : null;
+                    })
+            });
+        }
+    ], callback);
 };
---
+++ test/core-test.js
@@ -0,0 +1,43 @@
+var expect = require('expect.js');
+
+describe('core local install', function () {
+	it('must not have errors', function (done) {
+		require('../index')({ path: './' }, function (err, result) {
+			expect(err).to.not.be.ok();
+			expect(result).to.be.ok()
+			done();
+		});
+	});
+
+	it('must have valid package results', function (done) {
+		require('../index')({ path: './' }, function (err, result) {
+			expect(result.package).to.be.eql({
+		        "name": "packity",
+		        "version": "0.0.0",
+		        "dependencies": {
+		            "async": "^1.4.2",
+		            "readdir": "^0.0.13",
+		            "lodash": "^3.10.1",
+		            "semver": "^5.0.3"
+		        },
+		        "devDependencies": {
+		            "expect.js": "^0.3.1",
+		            "mocha": "^2.3.3"
+		        }
+		    });
+			done();
+		});
+	});
+
+	it('must result having status', function (done) {
+		require('../index')({ path: './' }, function (err, result) {
+			expect(result.status).to.be.eql({
+		        "async": true,
+		        "readdir": true,
+		        "lodash": true,
+		        "semver": true
+		    });
+			done();
+		});
+	});
+});
