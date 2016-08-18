47f2b114b96115b772d723566ee691af14f461bc
Shamasis Bhattacharya <mail@shamasis.net>
2016-08-19 04:18:29 +0530

    Updated package for release 0.2.0

--- test/core-test.js
+++ test/core-test.js
@@ -13,7 +13,7 @@ describe('core local install', function () {
 		require('../index')({ path: './' }, function (err, result) {
 			expect(result.package).to.be.eql({
 		        "name": "packity",
-		        "version": "0.1.0",
+		        "version": "0.2.0",
 		        "dependencies": {
 		            "async": "^1.4.2",
 		            "colors": "^1.1.2",
